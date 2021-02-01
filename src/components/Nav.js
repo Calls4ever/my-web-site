67f9d3c331290e93c7b613897d08f2ca7301379b
callas4ever <thetseringnorbu@gmail.com>
2021-01-31 20:17:46 -0500

    new start

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+const Nav=()=>{
+    return(
+        <ul>
+            <li>
+                <a>Home</a>
+            </li>
+            <li>
+                <a>Projects</a>
+            </li>
+            <li>
+                <a>Experience</a>
+            </li>
+            <li>
+               <a>Education</a> 
+            </li>
+            <li>
+                <a>Contact</a>
+            </li>
+        </ul> 
+    )
+}
+export default Nav
